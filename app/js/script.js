cbda4837cc5bd44c7944d259af9caf777f073ef4
Cedric Dellisse <ced2595@hotmail.com>
2017-05-09 09:37:48 +0200

    Script modification

--- app/js/script.js
+++ app/js/script.js
@@ -4,15 +4,15 @@ $.fn.generateSkillsChart = function() {
         type: 'horizontalBar',
         responsive: true,
         data: {
-            labels: ["Front-end", "Back-end", "Networking", "Programming"],
+            labels: ['Front-end', 'Back-end', 'Networking', 'Programming'],
             datasets: [
                 {
-                    label: "Skillset",
-                    backgroundColor: "rgba(255,99,132,0.2)",
-                    borderColor: "rgba(255,99,132,1)",
+                    label: 'Skillset',
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
                     borderWidth: 1,
-                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
-                    hoverBorderColor: "rgba(255,99,132,1)",
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
                     data: [70, 80, 95, 65]
                 }
             ]
@@ -47,5 +47,5 @@ $.fn.generateSkillsChart = function() {
 
 $(document).ready(function() {
 	// Generate skills chart if on about page
-	if (window.location.href.indexOf("about") > -1) $.fn.generateSkillsChart();
+	if (window.location.href.indexOf('about') > -1) $.fn.generateSkillsChart();
 });
